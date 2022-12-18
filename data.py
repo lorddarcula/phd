b3aaeb93ecc35f4ae2c90b5912cfac884876cf0c
lorddarcula <47804389+lorddarcula@users.noreply.github.com>
2022-12-18 17:56:26 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 query = "(elon OR musk OR parag OR agarwal OR twitter OR takeover OR fired OR buying OR shutdown) (#elonmusktwitter OR #elonmusktakeover OR #paragagarwal OR #twittershutdown) lang:en until:2022-11-10 since:2022-07-01 -filter:links -filter:replies"
 tweets = []
-limit = 5000
+limit = 100000
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     
     # print(vars(tweet))
